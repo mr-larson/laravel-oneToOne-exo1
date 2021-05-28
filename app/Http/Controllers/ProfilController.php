785279a526141ff24ef8f20c8f539d54f4ed0e10
gauthier dewit <gauthierdewit@gmail.com>
2021-05-28 17:40:00 +0200

    ordre migrations ok

---
+++ app/Http/Controllers/ProfilController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Profil;
+use Illuminate\Http\Request;
+
+class ProfilController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $profils = Profil::all();
+        return view('backoffice.profil.all', compact('profils'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('backoffice.profil.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $profil = new Profil();
+        $profil->name = $request->name;
+        $profil->age = $request->age;
+        $profil->phone = $request->phone;
+
+        $profil->save;
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Profil  $profil
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Profil $profil)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Profil  $profil
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Profil $profil)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Profil  $profil
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Profil $profil)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Profil  $profil
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Profil $profil)
+    {
+        //
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+    <h1 class="text-blue-600">hello</h1>
+@endsection
